b4e9779f58463f599447b97f204bd53594d73dca
Vaishnavisj <vaishnavisjoshi29@gmail.com>
2021-01-23 19:53:51 +0530

    added method to handle gdg version count

--- src/main/java/com/example/azuredemo/service/AzureDemoService.java
+++ src/main/java/com/example/azuredemo/service/AzureDemoService.java
@@ -1,5 +1,11 @@
 package com.example.azuredemo.service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.util.ArrayList;
@@ -20,7 +26,7 @@ import com.microsoft.azure.storage.blob.CloudBlobContainer;
 @Component
 public class AzureDemoService {
 	
-	public static final String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=vjdemo;"+"AccountKey=ClHTkRkOmo6JBSAs7Zpv1fCfmXSW0+sF7JIOys83+ZKftz90mlo5JeH1VxiL5Bamp4NcntWxTVF+n3UA7Vnq/A==;"+"EndpointSuffix=core.windows.net;";
+	public static final String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=vjdemo;"+"AccountKey=nq/A==;"+"EndpointSuffix=core.windows.net;";
 	
 	public long getTotalCountInContainer(String containerName)
 	 {
@@ -68,4 +74,68 @@ public class AzureDemoService {
 		 }
 		   return false;
 	}
+	
+	public List<String> getListOfBlobsInContainer()
+	{
+		 BlobServiceClient blobClient1 = new BlobServiceClientBuilder().connectionString(storageConnectionString).buildClient();
+		 BlobContainerClient containerClient = blobClient1.getBlobContainerClient("demo");
+		 List<String> blobNames = new ArrayList<String>();
+
+		 for(BlobItem blobItem : containerClient.listBlobs())
+		    {
+			 	blobNames.add(blobItem.getName());
+		    }
+		 return blobNames;
+	}
+	
+	public long getTotalCountUsingVersionFile(String containerName) throws IOException
+	{
+		 long count=0;
+		 File gdgVersion = new File("/Users/vaishnavi/Downloads/poc/src/main/resources/gdgVersion");
+		 BlobServiceClient blobClient = new BlobServiceClientBuilder().connectionString(storageConnectionString).buildClient();
+		 BlobContainerClient containerClient = blobClient.getBlobContainerClient(containerName);
+		 BlobClient blob = containerClient.getBlobClient("gdgVersion");
+		 if(!blob.exists())
+		 {
+			 count=1;
+			 if(gdgVersion.createNewFile())
+			 {
+				 System.out.println("gdgVersion file created ");
+			 }
+			 return count;
+			 
+		 }
+		 else
+		 {
+			 blob.downloadToFile("/Users/vaishnavi/Downloads/poc/src/main/resources/gdgVersion",true);
+			 FileReader fr=new FileReader(gdgVersion); 
+			 BufferedReader br = new BufferedReader(fr);  
+			 String line;  
+			 while((line=br.readLine())!=null)
+			 {
+				 count = Long.parseLong(line);
+	 
+			 }
+		 }
+		return count;
+	}
+	
+	public boolean updateGdgVersion() throws IOException
+	{
+		 BlobClient blobGdgVersion = getBlobFromContainer("gdgVersion", "demo");
+		 File gdgVersion = new File("/Users/vaishnavi/Downloads/poc/src/main/resources/gdgVersion");
+		 gdgVersion.setWritable(true);
+		 FileWriter fileWriter = new FileWriter(gdgVersion);
+		 long count = getTotalCountUsingVersionFile("demo");
+		 String strcount = String.valueOf(count);
+		 BufferedWriter writer = new BufferedWriter(fileWriter);
+		 fileWriter.write(strcount);
+		 fileWriter.flush();
+		 fileWriter.close();
+		 writer.close();
+		 blobGdgVersion.uploadFromFile("/Users/vaishnavi/Downloads/poc/src/main/resources/gdgVersion",true); 
+		 return true;
+
+	}
+	
 }
